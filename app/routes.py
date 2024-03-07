5b8268ce68bcf49a2f7c8bec8961cca49959a171
Kartik Sethi <kartik.2024it1184@kiet.edu>
2024-03-07 08:22:48 +0530

    ks/bug_fix

--- app/routes.py
+++ app/routes.py
@@ -17,6 +17,7 @@ def allowed_file(filename):
 def index():
     summarizer = Summarizer(nlp)
     form = SubmitTextForm(size=600)
+ 
 
    
         
@@ -45,28 +46,29 @@ def index():
 
         elif form.validate_on_submit():
             text = request.form['text']
-            num_sentences = int(request.form['num_sentences'])
+            if text != None:
+              num_sentences = int(request.form['num_sentences'])
               # Detect language
-            translator = Translator()
-            detected_lang = translator.detect(text).lang
+              translator = Translator()
+              detected_lang = translator.detect(text).lang
             
-            # Translate to English if not in English
-            if detected_lang != 'en':
+              # Translate to English if not in English
+              if detected_lang != 'en':
                 translated_text = translator.translate(text, src=detected_lang, dest='en').text
-            else:
+              else:
                 translated_text = text
-            word_weights, sentence_weights, sents, summary = summarizer.summarize_text(translated_text, num_sentences)
-            top_five_words = sorted(word_weights, key=word_weights.get, reverse=True)[:5]
-            sentence_weights = [value for key, value in sentence_weights.items()]
-            weighted_sentence_weights = [value/max(sentence_weights) for value in sentence_weights]
-            sentences_with_weights = list(zip(sents, weighted_sentence_weights))
-            target_language = request.form['target_language']  
-            translated_summary = translate_text(summary, target_language)
-            if 'extract_topics' in request.form:
-             text = request.form['text']
-             topics = extract_topics(translated_summary)  # Call your topic extraction function
-             return render_template('topics.html', topics=topics)
-            return render_template('summary.html',  text=translated_summary, top_words=top_five_words, sentence_weights=sentence_weights, sents=sentences_with_weights)
+              word_weights, sentence_weights, sents, summary = summarizer.summarize_text(translated_text, num_sentences)
+              top_five_words = sorted(word_weights, key=word_weights.get, reverse=True)[:5]
+              sentence_weights = [value for key, value in sentence_weights.items()]
+              weighted_sentence_weights = [value/max(sentence_weights) for value in sentence_weights]
+              sentences_with_weights = list(zip(sents, weighted_sentence_weights))
+              target_language = request.form['target_language']  
+              translated_summary = translate_text(summary, target_language)
+              if 'extract_topics' in request.form:
+               text = request.form['text']
+               topics = extract_topics(translated_summary)  # Call your topic extraction function
+               return render_template('topics.html', topics=topics)
+              return render_template('summary.html',  text=translated_summary, top_words=top_five_words, sentence_weights=sentence_weights, sents=sentences_with_weights)
 
     return render_template('index.html', text='', form=form)
 
